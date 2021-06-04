517512cd868146998b77bc5c89c14c5675034fd2
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-06-03 17:25:53 -0700

    linting __init__.py

--- mindscope_utilities/__init__.py
+++ mindscope_utilities/__init__.py
@@ -1,4 +1,4 @@
-from .general_utilities import *
-from .plotting_utilities import *
+from .general_utilities import *  # noqa: F401,F403
+from .plotting_utilities import *  # noqa: F401,F403
 
 __version__ = "0.0.0"
