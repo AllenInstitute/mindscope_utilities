c2bdc5fa06360669032e3ffc3625ecea02b06db3
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-06-15 09:17:33 -0700

    fix email format

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='mindscope_utilities',
     description='Utilities for loading, manipulating and visualizing data from the Allen Institute Mindscope program',
     url='https://github.com/AllenInstitute/mindscope_utilities',
     author='Allen Institute',
-    author_email='marinag, iryna.yavorska, kater, dougo, @alleninstitute.org',
+    author_email='marinag@alleninstitute.org, iryna.yavorska@alleninstitute.org, kater@alleninstitute.org, dougo@alleninstitute.org,',
     license='Allen Institute',
     install_requires=[
         'flake8',
