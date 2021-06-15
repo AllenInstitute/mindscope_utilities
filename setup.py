68664694a1b577e18505293d1398c6cfce724bba
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-06-15 10:05:54 -0700

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mindscope_utilities',
-    version='0.1.1',
+    version='0.1.2',
     description='Utilities for loading, manipulating and visualizing data from the Allen Institute Mindscope program',
     url='https://github.com/AllenInstitute/mindscope_utilities',
     author='Allen Institute',
@@ -12,4 +12,12 @@ setup(name='mindscope_utilities',
         'pytest',
         'allensdk==2.11.2',
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: Other/Proprietary License', # Allen Institute Software License
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8'
+  ],
 )
