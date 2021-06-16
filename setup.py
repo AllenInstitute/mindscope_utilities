6b0389f5db60331152b197a0450477c2850d444c
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-06-16 14:01:44 -0700

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mindscope_utilities',
-    version='0.1.3',
+    version='0.1.4',
     packages=['mindscope_utilities'],
     include_package_data = True,
     description='Utilities for loading, manipulating and visualizing data from the Allen Institute Mindscope program',
