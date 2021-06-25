8424bb662be369807c0219cafcd8e8c706c63af5
Doug Ollerenshaw <dougo@alleninstitute.org>
2021-06-25 07:47:32 -0700

    increment version to 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='mindscope_utilities',
-    version='0.1.5',
+    version='0.1.6',
     packages=['mindscope_utilities'],
     include_package_data = True,
     description='Utilities for loading, manipulating and visualizing data from the Allen Institute Mindscope program',
