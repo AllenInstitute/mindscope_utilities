1a927b408c31becc3d3a3789823d2787bb22b164
iryna.yavorska <irynayavorska@gmail.com>
2021-08-09 16:36:51 -0700

    changing to timestamps, #3

--- mindscope_utilities/general_utilities.py
+++ mindscope_utilities/general_utilities.py
@@ -71,7 +71,7 @@ def event_triggered_response(data, t, y, event_timestamps, t_before=1, t_after=1
     Now build a dataframe with one column for time,
     and another column that is a noise-corrupted sinuosoid with period of 1
     >>> data = pd.DataFrame({
-            'time': t,
+            'timestamps': t,
             'noisy_sinusoid': np.sin(2*np.pi*t) + np.random.randn(len(t))*3
         })
 
@@ -82,7 +82,7 @@ def event_triggered_response(data, t, y, event_timestamps, t_before=1, t_after=1
     starting at 0, since our period here is 1
     >>> etr = event_triggered_response(
             data,
-            x = 'time',
+            x = 'timestamps',
             y = 'noisy_sinusoid',
             event_timestamps = np.arange(100),
             t_before = 1,
@@ -96,16 +96,16 @@ def event_triggered_response(data, t, y, event_timestamps, t_before=1, t_after=1
     >>> fig, ax = plt.subplots()
     >>> sns.lineplot(
             data = etr,
-            x='time',
+            x='timestamps',
             y='noisy_sinusoid',
             ax=ax
         )
     '''
-    # set up a dictionary with key 'time' and
-    # value as a linearly spaced time array
+    # set up a dictionary with key 'timestamps' and
+    # value as a linearly spaced timestamps array
     step_size = 1/output_sampling_rate
     data_dict = {
-        'time': np.linspace(
+        'event_timestamps': np.linspace(
             -t_before,
             t_after,
             int((t_before + t_after) / step_size + int(include_endpoint)),
@@ -116,20 +116,20 @@ def event_triggered_response(data, t, y, event_timestamps, t_before=1, t_after=1
     # iterate over all event timestamps
     data_time_indexed = data.set_index(t, inplace=False)
 
-    for event_number, event_time in enumerate(np.array(event_timestamps)):
+    for event_number, event_timestamp in enumerate(np.array(event_timestamps)):
 
         # get a slice of the input data surrounding each event time
-        data_slice = data_time_indexed[y].loc[event_time - t_before: event_time + t_after]  # noqa: E501
+        data_slice = data_time_indexed[y].loc[event_timestamp - t_before: event_timestamp + t_after]  # noqa: E501
 
         # update our dictionary to have a new key defined as
         # 'event_{EVENT NUMBER}_t={EVENT TIME}' and
         # a value that includes an array that represents the
         # sliced data around the current event, interpolated
-        # on the linearly spaced time array
+        # on the linearly spaced timestamps array
         data_dict.update({
-            'event_{}_t={}'.format(event_number, event_time): np.interp(
-                data_dict['time'],
-                data_slice.index - event_time,
+            'event_{}_t={}'.format(event_number, event_timestamp): np.interp(
+                data_dict['timestamps'],
+                data_slice.index - event_timestamp,
                 data_slice.values
             )
         })
@@ -138,20 +138,20 @@ def event_triggered_response(data, t, y, event_timestamps, t_before=1, t_after=1
     wide_etr = pd.DataFrame(data_dict)
     if output_format == 'wide':
         # return the wide dataframe if output_format is 'wide'
-        return wide_etr.set_index('time')
+        return wide_etr.set_index('timestamps')
     elif output_format == 'tidy':
         # if output format is 'tidy',
         # transform the wide dataframe to tidy format
-        # first, melt the dataframe with the 'id_vars' column as "time"
-        tidy_etr = wide_etr.melt(id_vars='time')
+        # first, melt the dataframe with the 'id_vars' column as "timestamps"
+        tidy_etr = wide_etr.melt(id_vars='timestamps')
 
         # add an "event_number" column that contains the event number
         tidy_etr['event_number'] = tidy_etr['variable'].map(
             lambda s: s.split('event_')[1].split('_')[0]
         ).astype(int)
 
-        # add an "event_time" column that contains the event time ()
-        tidy_etr['event_time'] = tidy_etr['variable'].map(
+        # add an "event_timestamps" column that contains the event timestamps ()
+        tidy_etr['event_timestamps'] = tidy_etr['variable'].map(
             lambda s: s.split('t=')[1]
         ).astype(float)
 
