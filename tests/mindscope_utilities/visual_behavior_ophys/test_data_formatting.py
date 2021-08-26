70d8311aaf3fe1708c5e2c32789f5406f98e8d76
yavorska-iryna <irynayavorska@gmail.com>
2021-08-25 17:51:27 -0700

    linting

--- tests/mindscope_utilities/visual_behavior_ophys/test_data_formatting.py
+++ tests/mindscope_utilities/visual_behavior_ophys/test_data_formatting.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from mindscope_utilities import visual_behavior_ophys
 import allensdk.brain_observatory.behavior.behavior_project_cache as cache
 
@@ -9,7 +8,8 @@ def test_build_tidy_cell_df(simulated_experiment_fixture):
     tests visual_behavior_ophys.data_formatting.build_tidy_cell_df
     uses `simulated_experiment_fixture` defined in conftest.py
     '''
-    tidy_cell_df = visual_behavior_ophys.build_tidy_cell_df(simulated_experiment_fixture)
+    tidy_cell_df = visual_behavior_ophys.build_tidy_cell_df(
+        simulated_experiment_fixture)
 
     ans_1 = pd.DataFrame([{
         'timestamps': 0.5,
@@ -19,11 +19,15 @@ def test_build_tidy_cell_df(simulated_experiment_fixture):
         'events': 0.0,
         'filtered_events': 1.0
     }])
-    ans_1['cell_specimen_id'] = pd.Categorical(ans_1['cell_specimen_id'], categories=[1, 2, 3])
-    ans_1['cell_roi_id'] = pd.Categorical(ans_1['cell_roi_id'], categories=[2, 4, 6])
-    
-    cols = ['timestamps', 'cell_roi_id', 'cell_specimen_id', 'dff', 'events', 'cell_specimen_id']
-    actual_1 = tidy_cell_df.query('cell_specimen_id == 1 and timestamps == 0.5').reset_index(drop=True)
+    ans_1['cell_specimen_id'] = pd.Categorical(
+        ans_1['cell_specimen_id'], categories=[1, 2, 3])
+    ans_1['cell_roi_id'] = pd.Categorical(
+        ans_1['cell_roi_id'], categories=[2, 4, 6])
+
+    cols = ['timestamps', 'cell_roi_id', 'cell_specimen_id',
+            'dff', 'events', 'cell_specimen_id']
+    actual_1 = tidy_cell_df.query(
+        'cell_specimen_id == 1 and timestamps == 0.5').reset_index(drop=True)
     pd.testing.assert_frame_equal(actual_1[cols], ans_1[cols])
 
     ans_2 = pd.DataFrame([{
@@ -34,16 +38,19 @@ def test_build_tidy_cell_df(simulated_experiment_fixture):
         'events': 1.0,
         'filtered_events': 0.0
     }])
-    ans_2['cell_specimen_id'] = pd.Categorical(ans_2['cell_specimen_id'], categories=[1, 2, 3])
-    ans_2['cell_roi_id'] = pd.Categorical(ans_2['cell_roi_id'], categories=[2, 4, 6])
-    actual_2 = tidy_cell_df.query('cell_specimen_id == 2 and timestamps == 0.5').reset_index(drop=True)
+    ans_2['cell_specimen_id'] = pd.Categorical(
+        ans_2['cell_specimen_id'], categories=[1, 2, 3])
+    ans_2['cell_roi_id'] = pd.Categorical(
+        ans_2['cell_roi_id'], categories=[2, 4, 6])
+    actual_2 = tidy_cell_df.query(
+        'cell_specimen_id == 2 and timestamps == 0.5').reset_index(drop=True)
     pd.testing.assert_frame_equal(actual_2[cols], ans_2[cols])
+
 
 def test_get_event_timestamps(experiment_id):
     ophys_experiment = cache.get_behavior_ophys_experiment(experiment_id)
     stimulus_presentations = ophys_experiment.stimulus_presentations
     timestamp = 322.78641
-    event_times = ophys.get_event_timestamps(stimulus_presentations, 'changes')
-    asset(event_times[0], timestamp)
-    
-    
+    event_times = visual_behavior_ophys.get_event_timestamps(
+        stimulus_presentations, 'changes')
+    assert(event_times[0] == timestamp)
